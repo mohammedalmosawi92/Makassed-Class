513a041f197c15351a68ad321d1c7545f195bffe
mohammedalmosawi92 <messibarcelona92@gmail.com>
2018-02-21 22:09:57 +0200

    updated the dom full name script

--- Exercises/DOM_Full_Name/script.js
+++ Exercises/DOM_Full_Name/script.js
@@ -1,11 +1,12 @@
+var displayName = document.getElementById("displayName");
 function submitBtn() {
     var firstName = document.getElementById("fname").value;
     var lastName = document.getElementById("lname").value;
     var fullName = firstName + " " + lastName;
-    // 1- create new element ex: h1, div
-    var h1 = document.createElement("h1");
-    // 2- put fullName inside the new element
-    h1.innerHTML = fullName;
-    // 3- append the new element inside the div(id=displayName)
-    document.getElementById("displayName").appendChild(h1)
+    //1- create new element ex: h1, div, p using createElement("")
+    var header = document.createElement("h1");
+    //2- put fullName inside the new element using innerHTML
+    header.innerHTML = fullName;
+    //3- append the the new element inside the displayName using appendChild()
+    displayName.appendChild(header);
 }
